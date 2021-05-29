d9648f093a977449884f514cd6759bbd08f21430
zainsci <60652827+zainsci@users.noreply.github.com>
2021-05-29 11:58:12 +0500

    feat: Add Navbar Styling

--- site/components/header.js
+++ site/components/header.js
@@ -1,7 +1,45 @@
 import React from "react";
 
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header>
+      <nav className="nav navbar">
+        <div className="brand">DS</div>
+        <ul className="nav__list">
+          <li className="nav__item">
+            <a href="/" className="nav__link active">
+              Home
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="/colors" className="nav__link">
+              Colors
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="/type" className="nav__link">
+              Typography
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="/spacing" className="nav__link">
+              Spacing
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="/form" className="nav__link">
+              Forms
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="/docs" className="nav__link">
+              Docs
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
