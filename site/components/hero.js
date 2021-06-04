85ac9a58a40021fdf9b7739da6410bae3301c2df
zainsci <60652827+zainsci@users.noreply.github.com>
2021-06-04 12:39:21 +0500

    style: reimplement flexbox grid with gutters

--- site/components/hero.js
+++ site/components/hero.js
@@ -4,8 +4,8 @@ const Hero = () => {
   return (
     <>
       <div className="container">
-        <div className="py-20 grid">
-          <div className="p-4 col-1">
+        <div className="py-20 grid g-md">
+          <div className="p-4 col-4">
             <h1>Design System</h1>
             <p>
               An experimental Design System with modern design principles and
@@ -14,7 +14,7 @@ const Hero = () => {
             </p>
             <button className="btn btn-blue">Documentation</button>
           </div>
-          <div className="p-4 col-1">
+          <div className="p-4 col-8">
             <img src="/assets/svgs/main.svg" alt="responsive web" />
           </div>
         </div>
